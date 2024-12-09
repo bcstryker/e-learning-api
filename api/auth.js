643078fc17e360cbdbedf11962e919d8d4f127e8
Brandon Stryker <bcstryker@gmail.com>
2024-12-08 20:11:27 -0500

    fixed auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
-import connectDB from '../../config/db';
-import User from '../../models/User';
+import connectDB from '../config/db';
+import User from '../models/User';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
