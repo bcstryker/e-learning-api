f432bb0cf5a908cc5cd79fce429a5162115629c8
Brandon Stryker <bcstryker@gmail.com>
2024-12-09 14:12:55 -0500

    still trying to fix cors blocking

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 const { connectDB } = require("./config/db");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 const PORT = process.env.PORT || 5000;
 
 // Middleware
