52d97af8bc8449f73e652676907ed701983c826d
Brandon Stryker <bcstryker@gmail.com>
2024-12-12 16:04:59 -0500

    fixed preflight hopefully 2

--- api/courses.js
+++ api/courses.js
@@ -7,6 +7,10 @@ import User from "../models/User.js";
 export default async function handler(req, res) {
   await connectDB();
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
